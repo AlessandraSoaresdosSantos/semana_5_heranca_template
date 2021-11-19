dcf9828d3ac4c024e13a72a77a85f49eae4f6746
Alessandra Soares dos Santos <AlessandraSoaresdosSantos@users.noreply.github.com>
2021-11-18 21:37:48 -0300

    correção

---
+++ src/services/js/script.js
@@ -0,0 +1,50 @@
+
+ const formPessoa = document.querySelector('#formulario-pessoa')
+ const listaPessoas = document.querySelector('#lista-pessoas')
+  
+ const historico = []
+ 
+ function registrarNovaPessoa(evento) {
+   
+    const dadosDoForm = new FormData(evento.target)
+
+    const nome = dadosDoForm.get('nome')
+    const endereco = dadosDoForm.get('endereco')
+ 
+ }
+
+
+
+
+
+
+
+
+
+ function atualizarTela() {
+    listaPessoas.innerHTML = ''
+
+    for (let pessoa of historico) {
+        const item = document.createElement('li')
+
+        item.innerText = `
+      Nome: ${pessoa.nome}
+      
+    `
+
+        listaPessoas.appendChild(item)
+    }
+}
+
+formPessoa.addEventListener('submit', registrarNovaPessoa)
+
+document.querySelector('#t-btn-limpar').addEventListener('click', () => {
+    formPessoa.reset()
+
+    for (let i = historico.length; i > 0; i--) {
+        historico.pop()
+    }
+
+    atualizarTela()
+})
+  
